ab06480f583679f458c8661672aba355c05bff58
Zedkays <76875658+Zedkays@users.noreply.github.com>
2022-08-06 13:54:50 +0100

    Update __init__.lua

--- __init__.lua
+++ __init__.lua
@@ -1,1 +1,9 @@
+players = {}
+
+function player_connected(player)
+    table.insert(players, player) 
+end
+
+level:onnotify("connected", player_connected)
+
 level:onnotify("connected", mapvoteconnected)
